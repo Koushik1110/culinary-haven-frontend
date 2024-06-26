052b1c40d010521b70b4950582639b0c461cacd3
Koushik1110 <148125270+Koushik1110@users.noreply.github.com>
2024-06-26 14:41:16 +0600

    apos solved

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,10 @@
-// pages/about.js
-
 const AboutPage = () => {
   return (
     <div className="bg-gray-100 min-h-screen flex items-center justify-center text-center">
       <div className="bg-white p-8 rounded-xl shadow-md w-10/12">
-        <h1 className="text-5xl text-orange-600 font-semibold mb-4">About Us</h1>
+        <h1 className="text-5xl text-orange-600 font-semibold mb-4">
+          About Us
+        </h1>
         <p className="text-orange-400 text-xl">
           Welcome to our food ordering platform! We are passionate about
           bringing delicious meals right to your doorstep. Our team of chefs and
@@ -12,8 +12,8 @@ const AboutPage = () => {
           to all tastes and preferences.
         </p>
         <p className="mt-4 text-orange-400 text-xl">
-          Whether you're craving comfort food, exotic flavors, or healthy
-          options, we've got you covered. Explore our menu, place your order,
+          Whether you&apos;re craving comfort food, exotic flavors, or healthy
+          options, we&apos;ve got you covered. Explore our menu, place your order,
           and let us take care of the rest!
         </p>
       </div>
